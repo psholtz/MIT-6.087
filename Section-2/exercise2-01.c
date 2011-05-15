8a2c31839b9d8eebfc2af05daa00146eb470e8ed
psholtz <paul.sholtz@gmail.clom>
2011-05-14 23:06:52 -0700

    fixing ansewr

--- Section-2/exercise2-01.c
+++ Section-2/exercise2-01.c
@@ -1,4 +1,8 @@
 /**
+ * Problem 2.1: Determine the size, minimum and maximum value of the following
+ * data types. Please specify if your machine is 32 bit or 64 bits in the answer.
+ *
+ * Solution: The sizes of the data types is given below:
  *
  *  byte:          1
  *  unsigned char: 1
@@ -10,19 +14,28 @@
  *  double:        8
  *
  * Machine has a 32-bit architecture.
+ *
+ * Importing the limits.h header file, we get the following values for these constants:
+ *
+ *  - LONG_MIN: -2147384648
+ *  - LONG_MAX: 2147483647
+ *  - ULONG_MAX: 4294967295
+ *
+ * These are the values that correspond to a 32-bit architecture.
  */
 #include <stdio.h>
 #include <limits.h>
+#include <float.h>
 
 int main(void) {
-  printf("byte: \t\t%ld\n",sizeof(char));
-  printf("unsigned char: \t%ld\n",sizeof(unsigned char));
-  printf("short: \t\t%ld\n",sizeof(short));
-  printf("int: \t\t%ld\n",sizeof(int));
-  printf("unsigned int: \t%ld\n",sizeof(unsigned int));
-  printf("unsigned long: \t%ld\n",sizeof(unsigned long));
-  printf("float: \t\t%ld\n",sizeof(float));
-  printf("double: \t%ld\n",sizeof(double));
+  printf("char: \t\t%ld %d %d\n",sizeof(char),CHAR_MIN,CHAR_MAX);
+  printf("unsigned char: \t%ld %d %d\n",sizeof(unsigned char),0,UCHAR_MAX);
+  printf("short: \t\t%ld %d %d\n",sizeof(short),SHRT_MIN,SHRT_MAX);
+  printf("int: \t\t%ld %d %d\n",sizeof(int),INT_MIN,INT_MAX);
+  printf("unsigned int: \t%ld %d %u\n",sizeof(unsigned int),0,UINT_MAX);
+  printf("unsigned long: \t%ld %d %lu\n",sizeof(unsigned long),0,ULONG_MAX);
+  printf("float: \t\t%ld %e %e\n",sizeof(float),FLT_MIN,FLT_MAX);
+  printf("double: \t%ld %e %e\n",sizeof(double),DBL_MIN,DBL_MAX);
 
   printf("\n");
 

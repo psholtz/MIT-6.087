adcc15056d2a44cf7a0a67d76719ddcf72998386
psholtz <paul.sholtz@gmail.clom>
2011-05-15 07:33:10 -0700

    addig exercise

--- Section-2/exercise2-01.c
+++ Section-2/exercise2-01.c
@@ -4,14 +4,15 @@
  *
  * Solution: The sizes of the data types is given below:
  *
- *  byte:          1
- *  unsigned char: 1
- *  short:         2
- *  int:           4
- *  unsigned int:  4
- *  unsigned long: 4
- *  float:         4
- *  double:        8
+ *  byte:          1 -128 127
+ *  unsigned char: 1 0 255
+ *  short:         2 -32768 32767
+ *  short:         2 0 65535
+ *  int:           4 -2147483648 2147483647
+ *  unsigned int:  4 0 4294967295
+ *  unsigned long: 4 0 4294967295
+ *  float:         4 1.75494e-38 3.402823e+38
+ *  double:        8 2.225074e-308 1.797693e+308
  *
  * Machine has a 32-bit architecture.
  *
@@ -23,6 +24,7 @@
  *
  * These are the values that correspond to a 32-bit architecture.
  */
+
 #include <stdio.h>
 #include <limits.h>
 #include <float.h>
@@ -31,6 +33,7 @@ int main(void) {
   printf("char: \t\t%ld %d %d\n",sizeof(char),CHAR_MIN,CHAR_MAX);
   printf("unsigned char: \t%ld %d %d\n",sizeof(unsigned char),0,UCHAR_MAX);
   printf("short: \t\t%ld %d %d\n",sizeof(short),SHRT_MIN,SHRT_MAX);
+  printf("unsigned short: %ld %d %d\n",sizeof(unsigned short),0,USHRT_MAX);
   printf("int: \t\t%ld %d %d\n",sizeof(int),INT_MIN,INT_MAX);
   printf("unsigned int: \t%ld %d %u\n",sizeof(unsigned int),0,UINT_MAX);
   printf("unsigned long: \t%ld %d %lu\n",sizeof(unsigned long),0,ULONG_MAX);

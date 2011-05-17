5c7d077619a9f5e079077e2ac10c90f75d0a3463
psholtz <paul.sholtz@gmail.com>
2011-05-17 09:27:32 -0700

    adding

---
+++ Section-3/exercise3-01.c
@@ -0,0 +1,24 @@
+/**
+ *
+ */
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+#define NMAX 25
+static unsigned int results_buffer[NMAX];
+
+void fibonacci()
+{
+  /* here are the variables to set as registers */
+  unsigned int a = 0;
+  unsigned int b = 1;
+  unsigned int c;
+  int n;
+}
+
+int main(void) {
+
+  return 0;
+}
